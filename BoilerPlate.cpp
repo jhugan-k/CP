d3772612b9ae7ea74db2dc82731dbaf422d0d570
Jhugan Kartikey <jkartikey.official@gmail.com>
2025-08-11 22:54:14 +0530

    Boilerplate code

---
+++ BoilerPlate.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+class Solution{
+    public:
+    
+    int func(){
+
+    }
+};
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    Solution sol;
+    //cin-cout
+    return 0;
+}
